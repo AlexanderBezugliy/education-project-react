8c1265168b2be9ac9b8488ca070c34633338ebcd
AlexanderBezugliy <alexanderbezugliy92@gmail.com>
2025-12-19 19:31:53 +0200

    About & Campus

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,28 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
+import Campus from './components/Campus/Campus'
+import Testimonials from './components/Testimonials/Testimonials'
 
 const App = () => {
     return (
         <>
             <Navbar />
             <Hero />
+
             <div className='container'>
+                <Title  title="Our PROGRAM" subTitle="What We Offer" />
                 <Programs />
+                <About />
+                <Title  title="Gallery" subTitle="Campus Photos" />
+                <Campus/>
+                <Title  title="TESTIMONIALS" subTitle="What Student Says" />
+                <Testimonials />
+
             </div>
+
         </>
     )
 }
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Testimonials.css'
+import next_icon from '../../assets/next-icon.png'
+
+
+
+const Testimonials = () => {
+    return (
+        <div className='testimonials'>
+            <img src={next_icon} alt="" className='next-btn' />
+        </div>
+    )
+}
+
+export default Testimonials
